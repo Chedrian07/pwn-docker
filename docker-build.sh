e8158052a2dac43da2fa717afcdc11289c3998e0
Chedrian07 <kch3dri4n@M1-MAXui-MacBookPro.local>
2024-01-13 00:05:48 +0900

    First-My-Pwn-Enviroment2

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
-docker build -t pwn1804 -f 1804/Dockerfile .                            
-docker build -t pwn2004 -f 2004/Dockerfile .
-docker build -t pwn2204 -f 2204/Dockerfile .
+docker build -t pwn1804 -f 1804/Dockerfile . &                      
+docker build -t pwn2004 -f 2004/Dockerfile . &
+docker build -t pwn2204 -f 2204/Dockerfile . &
