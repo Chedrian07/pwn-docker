b835a53512d3efd29243b4ebd593d887eefafbb3
Chedrian07 <seungjung0711@gmail.com>
2024-07-15 03:53:19 +0900

    2024.07.15

---
+++ build.bat
@@ -0,0 +1,5 @@
+docker build -t pwn-ubuntu:16.04 ./pwn1604/
+docker build -t pwn-ubuntu:18.04 ./pwn1804/
+docker build -t pwn-ubuntu:20.04 ./pwn2004/
+docker build -t pwn-ubuntu:22.04 ./pwn2204/
+docker build -t pwn-ubuntu:24.04 ./pwn2404/
